b7c7c580b0dd4e2ec7a85217529f52d73fb5f036
Braden Smelser <bsmelser@odin.cs.csubak.edu>
2024-02-28 19:04:09 -0800

    Decided to switch to the C language, better examples to go off of inlcuding cmps 3600

---
+++ game.c
@@ -0,0 +1,126 @@
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/keysym.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, obstacleX, obstacleY, score;
+enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
+int spaceshipX = 100;
+int spaceshipY = 100;
+
+struct Global {
+    Display *dpy;
+    Window win;
+    GC gc;
+    int xres, yres;
+} g;
+
+void x11_cleanup_xwindows(void);
+void x11_init_xwindows(void);
+void x11_clear_window(void);
+void check_mouse(XEvent *e);
+int check_keys(XEvent *e);
+void render(void);
+void pan();
+
+void pan()
+{
+    XClearWindow(g.dpy, g.win);
+
+    // Draw border
+    XDrawRectangle(g.dpy, g.win, g.gc, 0, 0, width * 20, height * 20);
+
+    // Draw spaceship
+    XFillRectangle(g.dpy, g.win, g.gc, x * 20, y * 20, 20, 20);
+
+    // Draw obstacle
+    XFillRectangle(g.dpy, g.win, g.gc, obstacleX * 20, obstacleY * 20, 20, 20);
+
+    // Bottom of screen (location of spaceship)
+    XFillRectangle(g.dpy, g.win, g.gc, spaceshipX, spaceshipY, 20, 20);
+
+    XFlush(g.dpy);
+}
+ 
+void render(void)
+{
+        XClearWindow(g.dpy, g.win);
+
+        XSetFont(g.dpy, g.gc, XLoadFont(g.dpy, "9x15bold"));
+
+        XSetForeground(g.dpy, g.gc, 0xffffff);
+        XFillRectangle(g.dpy, g.win, g.gc, 0, 0, g.xres, g.yres);
+
+        XSetForeground(g.dpy, g.gc, 0x48494B);
+        XDrawString(g.dpy, g.win, g.gc, 125, 20, "GoldRushLite", 12);
+
+        XSetForeground(g.dpy, g.gc, 0x48494B);
+        XDrawString(g.dpy, g.win, g.gc, 1, 20, "Timer: ", 7);
+
+        XSetForeground(g.dpy, g.gc, 0x48494B);
+        XDrawString(g.dpy, g.win, g.gc, 300, 20, "Score: ", 7);
+        
+        XFlush(g.dpy);
+
+}
+
+void x11_setFont(unsigned int idx)
+{
+    char *fonts[] = { "fixed","5x8","6x9","6x10","6x12","6x13","6x13bold",
+        "7x13","7x13bold","7x14","8x13","8x13bold","8x16","9x15","9x15bold",
+        "10x20","12x24" };
+    Font f = XLoadFont(g.dpy, fonts[idx]);
+    XSetFont(g.dpy, g.gc, f);
+}
+
+void x11_cleanup_xwindows(void)
+{
+    XDestroyWindow(g.dpy, g.win);
+    XCloseDisplay(g.dpy);
+}
+
+void x11_init_xwindows(void)
+{
+    int scr;
+
+
+
+    if (!(g.dpy = XOpenDisplay(NULL))) {
+        fprintf(stderr, "ERROR: could not open display!\n");
+        exit(EXIT_FAILURE);
+    }
+    scr = DefaultScreen(g.dpy);
+    g.xres = 400;
+    g.yres = 200;
+    g.win = XCreateSimpleWindow(g.dpy, RootWindow(g.dpy, scr), 1, 1,
+                            g.xres, g.yres, 0, 0x00ffffff, 0x00000000);
+    XStoreName(g.dpy, g.win, "cs3600 xwin sample");
+    g.gc = XCreateGC(g.dpy, g.win, 0, NULL);
+    XMapWindow(g.dpy, g.win);
+    XSelectInput(g.dpy, g.win, ExposureMask | StructureNotifyMask |
+                                PointerMotionMask | ButtonPressMask |
+                                ButtonReleaseMask | KeyPressMask);
+}
+
+
+int main(){
+ x11_init_xwindows();
+    while (!gameOver) {
+        render();
+        XEvent e;
+        XNextEvent(g.dpy, &e);
+        if (e.type == Expose) {
+            // Redraw the window
+            render();
+        }
+    }
+x11_cleanup_xwindows();
+    return 0;
+}
